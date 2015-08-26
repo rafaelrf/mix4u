eab1b7e078bcf7d5d2f3dbeabbe4442cbb925326
Rafael Ribeiro Fontenele <rafaelrfontenele@gmail.com>
2015-08-26 03:24:43 -0300

    Angular methods

--- client/index.js
+++ client/index.js
@@ -1,20 +1,49 @@
-
 if (Meteor.isClient) {
-Tasks = new Mongo.Collection('tasks');
+    Tasks = new Mongo.Collection('tasks');
+
 
     angular.module('mix4u', ['angular-meteor']);
 
     angular.module('mix4u').controller('AppController', ['$scope', '$meteorCollection',
         function($scope, $meteor) {
 
-            $scope.tasks = $scope.$meteorCollection(Tasks,true);
+            $scope.tasks = $scope.$meteorCollection(function() {
+                return Tasks.find($scope.getReactively('query'), {
+                    sort: {
+                        createdAt: -1
+                    }
+                })
+            });
 
             $scope.addTask = function(newTask) {
-                $scope.tasks.push({
-                    text: newTask,
-                    createdAt: new Date()
-                });
+                $meteor.call('addTask', newTask);
             };
+
+            $scope.deleteTask = function(task) {
+                $meteor.call('deleteTask', task._id);
+            };
+
+            $scope.setChecked = function(task) {
+                $meteor.call('setChecked', task._id, !task.checked);
+            };
+            $scope.$watch('hideCompleted', function() {
+                if ($scope.hideCompleted)
+                    $scope.query = {
+                        checked: {
+                            $ne: true
+                        }
+                    };
+                else
+                    $scope.query = {};
+            });
+            $scope.incompleteCount = function() {
+                return Tasks.find({
+                    checked: {
+                        $ne: true
+                    }
+                }).count();
+            };
+
 
         }
     ]);
@@ -27,7 +56,7 @@ Tasks = new Mongo.Collection('tasks');
         addTask: function(text) {
             // Make sure the user is logged in before inserting a task
             if (!Meteor.userId()) {
-                throw new Meteor.Error("not-authorized");
+                throw new Meteor.Error('not-authorized');
             }
 
             Tasks.insert({
@@ -48,6 +77,4 @@ Tasks = new Mongo.Collection('tasks');
             });
         }
     });
-
 }
-
--- server/index.js
+++ server/index.js
@@ -1,7 +1,32 @@
-
 if (Meteor.isServer) {
-Tasks = new Mongo.Collection('tasks');
     Meteor.startup(function() {
         // code to run on server at startup
+        Tasks = new Mongo.Collection('tasks');
+    });
+
+    Meteor.methods({
+        addTask: function(text) {
+            // Make sure the user is logged in before inserting a task
+            if (!Meteor.userId()) {
+                throw new Meteor.Error('not-authorized');
+            }
+
+            Tasks.insert({
+                text: text,
+                createdAt: new Date(),
+                owner: Meteor.userId(),
+                username: Meteor.user().username
+            });
+        },
+        deleteTask: function(taskId) {
+            Tasks.remove(taskId);
+        },
+        setChecked: function(taskId, setChecked) {
+            Tasks.update(taskId, {
+                $set: {
+                    checked: setChecked
+                }
+            });
+        }
     });
 }
